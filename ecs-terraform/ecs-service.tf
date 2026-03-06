fdaa6b8e2e3d5be1d22a6b31fc02821441861dda
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 11:09:31 +0530

    Add ECS service configuration for webapp

---
+++ ecs-terraform/ecs-service.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_service" "app" {
+  name            = "webapp-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = aws_subnet.private[*].id
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "webapp"
+    container_port   = 80
+  }
+}
