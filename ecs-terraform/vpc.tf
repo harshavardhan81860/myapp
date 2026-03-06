f0d17ac974caf4ec7582d827fafba7875d8a7ab8
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 18:16:51 +0530

    Update vpc.tf

--- ecs-terraform/vpc.tf
+++ ecs-terraform/vpc.tf
@@ -21,3 +21,22 @@ resource "aws_subnet" "private" {
 }
 
 data "aws_availability_zones" "available" {}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
