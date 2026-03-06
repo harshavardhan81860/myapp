5611df7550051d51ba492ee8a032e0e2630205c8
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 18:21:40 +0530

    Update iam.tf

--- ecs-terraform/iam.tf
+++ ecs-terraform/iam.tf
@@ -1,41 +1,10 @@
-# 1. Create the IAM User for GitHub Actions
-resource "aws_iam_user" "github_actions" {
-  name = "github-actions-deployer"
+# This tells Terraform to bring the existing user into your code
+import {
+  to = aws_iam_user.github_actions
+  id = "github-actions-deployer"
 }
 
-# 2. Create a policy allowing GitHub to access ECR and ECS
-resource "aws_iam_user_policy" "github_deploy_policy" {
-  name = "github-deploy-policy"
-  user = aws_iam_user.github_actions.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecs:UpdateService",
-          "ecs:DescribeServices",
-          "ecs:RegisterTaskDefinition",
-          "ecs:DescribeTaskDefinition",
-          "iam:PassRole"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
+# You still need the resource block to define the user
+resource "aws_iam_user" "github_actions" {
+  name = "github-actions-deployer"
 }
