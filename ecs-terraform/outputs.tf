b5c65ac94cd51342aa22f8aaf34942924c7c00e2
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 17:18:34 +0530

    Update outputs.tf

--- ecs-terraform/outputs.tf
+++ ecs-terraform/outputs.tf
@@ -1,7 +1,12 @@
+# Fix for ALB DNS Output
 output "alb_dns" {
-  value = aws_lb.alb.dns_name
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.app_alb.dns_name
 }
 
+# Fix for ECR Repo URL Output
+# (Ensure your ECR resource in ecr.tf is named "app_repo")
 output "ecr_repo_url" {
-  value = aws_ecr_repository.repo.repository_url
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
 }
