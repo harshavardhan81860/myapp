39ec93ef4a0cbb1ec474f9026271677d108434ba
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 16:38:53 +0530

    Create vpc.tf

---
+++ ecs-terraform/vpc.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "main-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = { Name = "public-subnet-${count.index}" }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = { Name = "private-subnet-${count.index}" }
+}
+
+data "aws_availability_zones" "available" {}
