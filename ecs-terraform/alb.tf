cf9db89e41f6f575f6cdd2c8b6ae1bab12239061
harshavardhan81860 <harsha81860@gmail.com>
2026-03-06 11:18:56 +0530

    Update alb.tf

--- ecs-terraform/alb.tf
+++ ecs-terraform/alb.tf
@@ -1,19 +1,19 @@
 # 1. The Application Load Balancer
 resource "aws_lb" "app_alb" {
-  name               = "webapp-alb"
+  name               = "myapp-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id] # Ensure you have an ALB SG defined
   subnets            = aws_subnet.public[*].id
 
   tags = {
-    Name = "webapp-alb"
+    Name = "myapp-alb"
   }
 }
 
 # 2. The Target Group
 resource "aws_lb_target_group" "app_tg" {
-  name        = "webapp-tg"
+  name        = "myapp-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
