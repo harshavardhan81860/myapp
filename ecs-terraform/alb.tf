9e0969f1bf7735ffc81a9a66e2f5a910b83aea6d
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:55:11 +0530

    Update alb.tf

--- ecs-terraform/alb.tf
+++ ecs-terraform/alb.tf
@@ -1,21 +1,4 @@
-resource "aws_security_group" "alb_sg" {
-  name   = "alb-sg"
-  vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_lb" "alb" {
   name               = "myapp-alb"
