81ffb8396d9fc156aaf2bea4fcbf0fa814c0715c
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:30:26 +0530

    Update security.tf

--- ecs-terraform/security.tf
+++ ecs-terraform/security.tf
@@ -1,10 +1,24 @@
+############################
+# ALB Security Group
+############################
+
 resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
+  name        = "myapp-alb-sg"
+  description = "Allow HTTP from internet"
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "Allow HTTP traffic"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -14,5 +28,38 @@ resource "aws_security_group" "alb_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
+############################
+# ECS Service Security Group
+############################
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "myapp-ecs-sg"
+  description = "Allow traffic from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow ALB to reach ECS"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-security-group"
   }
 }
