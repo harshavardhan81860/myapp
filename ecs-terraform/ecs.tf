440363dfc6869fb73986aa30ac77b9a74e62316c
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 11:17:38 +0530

    Update ecs.tf

--- ecs-terraform/ecs.tf
+++ ecs-terraform/ecs.tf
@@ -23,26 +23,3 @@ resource "aws_ecs_task_definition" "task" {
     }
   ])
 }
-
-
-resource "aws_ecs_service" "service" {
-  name            = "myapp-task-service"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    assign_public_ip = true
-    security_groups  = [aws_security_group.ecs_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tg.arn
-    container_name   = "myapp"
-    container_port   = 80
-  }
-
-  depends_on = [aws_lb_listener.listener]
-}
