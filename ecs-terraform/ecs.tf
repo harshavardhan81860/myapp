8c0dd0777cb34c8db46de5cee894644e98a45da3
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:58:56 +0530

    Update ecs.tf

--- ecs-terraform/ecs.tf
+++ ecs-terraform/ecs.tf
@@ -24,24 +24,6 @@ resource "aws_ecs_task_definition" "task" {
   ])
 }
 
-resource "aws_ecs_security_group" "ecs_sg" {
-  name   = "ecs-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_ecs_service" "service" {
   name            = "myapp-task-service"
