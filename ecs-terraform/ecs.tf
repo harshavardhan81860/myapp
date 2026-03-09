5f7282e066c66338021599268576f3da13560ceb
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:29:47 +0530

    Update ecs.tf

--- ecs-terraform/ecs.tf
+++ ecs-terraform/ecs.tf
@@ -1,3 +1,15 @@
 resource "aws_ecs_cluster" "main" {
   name = "myapp-cluster"
+network_configuration {
+  subnets = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
+
+  security_groups = [
+    aws_security_group.ecs_sg.id
+  ]
+
+  assign_public_ip = true
+}
 }
