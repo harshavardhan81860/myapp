8d752c4b6bb17416a025eff7479d2295e545d88f
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:13:48 +0530

    Update ecs-service.tf

--- ecs-terraform/ecs-service.tf
+++ ecs-terraform/ecs-service.tf
@@ -5,10 +5,11 @@ resource "aws_ecs_service" "app" {
   launch_type     = "FARGATE"
   desired_count   = 1
 
+
 network_configuration {
   subnets          = aws_subnet.public[*].id
-  assign_public_ip = true 
-  security_groups  = [aws_security_group.ecs_tasks.id]
+  security_groups  = [aws_security_group.ecs_tasks_sg.id]
+  assign_public_ip = true  
 }
 
   load_balancer {
