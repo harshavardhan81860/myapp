dcde2491c62b18dce69e02c8aac9364f52b43209
harshavardhan81860 <harsha81860@gmail.com>
2026-03-09 10:47:38 +0530

    Update ecs.tf

--- ecs-terraform/ecs.tf
+++ ecs-terraform/ecs.tf
@@ -1,15 +1,66 @@
-resource "aws_ecs_cluster" "main" {
+resource "aws_ecs_cluster" "cluster" {
   name = "myapp-cluster"
-network_configuration {
-  subnets = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
-
-  security_groups = [
-    aws_security_group.ecs_sg.id
-  ]
-
-  assign_public_ip = true
 }
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "myapp-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "myapp"
+      image = "nginx"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "ecs-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "myapp-task-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "myapp"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.listener]
 }
