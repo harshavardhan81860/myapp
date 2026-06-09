7978e1b769c2cc830f034c00991c9093809c4993
harshavardhan81860 <harsha81860@gmail.com>
2026-06-10 00:01:28 +0530

    Update vpc.tf

--- ecs-terraform/vpc.tf
+++ ecs-terraform/vpc.tf
@@ -5,14 +5,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
 
