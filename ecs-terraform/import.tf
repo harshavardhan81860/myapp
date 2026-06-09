766ea4788c38e8643fdcecfb91e6de191daeea10
harshavardhan81860 <harsha81860@gmail.com>
2026-06-09 17:13:57 +0530

    Update import.tf

--- ecs-terraform/import.tf
+++ ecs-terraform/import.tf
@@ -1,4 +1,7 @@
-resource "aws_instance" "ardocd" {
-  ami           = "ami-0db56f446d44f2f09" # Put your instance ID here as a temporary placeholder
-  instance_type = "t3.micro"
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0db56f446d44f2f09"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0b7c5daf46a3eaf12"
+  
+  # Leaving it minimal for now so the import can link up
 }
